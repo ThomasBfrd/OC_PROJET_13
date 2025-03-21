aa5d6ebdc1d5eb1b41c6292c1770e1989ac224ee
ThomasBfrd <thomas.bfrd@gmail.com>
2025-03-21 15:49:02 +0100

    feat: gestion de l'état dans le cas où le login renvoie une erreur

--- src/core/userSlicer.ts
+++ src/core/userSlicer.ts
@@ -1,7 +1,8 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {User} from "./interfaces/user-interface.ts";
-import {editUserInfos, getUserInfos, getUserToken} from "./services/Api.tsx";
+import {updateUserInfos, fetchTokenUser, fetchUserInfos} from "./services/Api.tsx";
 import {UserLogin} from "./interfaces/user-login-interface.ts";
+import {EditUserInformations} from "./interfaces/user-edit-interface.ts";
 
 const initialState: User = {
     userInformation: {
@@ -14,12 +15,12 @@ const initialState: User = {
     rememberMe: false,
 }
 
-export const fetchTokenUser = createAsyncThunk(
+export const getUserToken = createAsyncThunk(
     "user/fetchTokenUser",
     async (userLoginBody: UserLogin, {rejectWithValue})=> {
         try {
             console.log('LoginBody : ', userLoginBody);
-            const response: any = await getUserToken(userLoginBody);
+            const response: any = await fetchTokenUser(userLoginBody);
 
             if (response.status === 200) {
                 console.log('Response UserToken : ', response);
@@ -35,12 +36,12 @@ export const fetchTokenUser = createAsyncThunk(
     }
 )
 
-export const fetchInfosUser = createAsyncThunk(
-    "user/fetchInfosUser",
+export const getUserInfos = createAsyncThunk(
+    "user/getUserInfos",
     async (userToken: string, {rejectWithValue})=> {
         try {
             console.log('LoginBody : ', userToken);
-            const response: any = await getUserInfos(userToken);
+            const response: any = await fetchUserInfos(userToken);
 
             if (response.status === 200) {
                 console.log('Response userInfos : ', response);
@@ -52,21 +53,15 @@ export const fetchInfosUser = createAsyncThunk(
     }
 )
 
-export interface EditUser {
-    userToken: string,
-    firstName: string,
-    lastName: string
-}
-
-export const fetchEditUserInfos = createAsyncThunk(
-    "user/fetchEditUserInfos",
-    async (query: EditUser, {rejectWithValue}) => {
+export const setUserInfos = createAsyncThunk(
+    "user/setUserInfos",
+    async (query: EditUserInformations, {rejectWithValue}) => {
         try {
             console.log('LoginBody : ', query.userToken);
-            const response: any = await editUserInfos(query);
+            const response: any = await updateUserInfos(query);
 
             if (response.status === 200) {
-                console.log('Response userInfos : ', response);
+                console.log('Response UserInfos : ', response);
                 return response;
             }
         } catch (error: any) {
@@ -86,23 +81,25 @@ const userSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder
-            .addCase(fetchTokenUser.pending, (_, action) => {
+            // GetTokenUser
+            .addCase(getUserToken.pending, (_, action) => {
                 console.log('Connexion en cours...', action.payload);
             })
-            .addCase(fetchTokenUser.fulfilled, (state, action) => {
+            .addCase(getUserToken.fulfilled, (state, action) => {
                 console.log('Connexion réussie', action.payload);
                 state.token = action.payload.body.token
 
                 console.log('Contenu du state mis à jour : ', state.token)
             })
-            .addCase(fetchTokenUser.rejected, (_, action) => {
-                console.log("Une erreur est survenue... ", action.payload);
+            .addCase(getUserToken.rejected, (_, action) => {
+                console.log("Une erreur est survenue... ", action);
             })
 
-            .addCase(fetchInfosUser.pending, (_, action) => {
+            // GetInfosUser
+            .addCase(getUserInfos.pending, (_, action) => {
                 console.log('Récupération des infos en cours...', action.payload);
             })
-            .addCase(fetchInfosUser.fulfilled, (state, action) => {
+            .addCase(getUserInfos.fulfilled, (state, action) => {
                 console.log('Récupération des infos réussie', action.payload);
                 state.userInformation = {
                     email: action.payload.body.email,
@@ -113,7 +110,27 @@ const userSlice = createSlice({
 
                 console.log('Contenu du state mis à jour pour userInfos: ', state.userInformation)
             })
-            .addCase(fetchInfosUser.rejected, (_, action) => {
+            .addCase(getUserInfos.rejected, (_, action) => {
+                console.log("Une erreur est survenue... ", action.payload);
+            })
+
+            // SetInfosUser
+            .addCase(setUserInfos.pending, (_, action) => {
+                console.log("Attente d'update du profile :", action.payload);
+            })
+
+            .addCase(setUserInfos.fulfilled, (state, action) => {
+                console.log('Update des infos user dans le slicer');
+                state.userInformation = {
+                    ...state.userInformation,
+                    firstName: action.payload.body.firstName,
+                    lastName: action.payload.body.lastName,
+                };
+
+                console.log('Contenu correctement mis à jour : ', state.userInformation);
+            })
+
+            .addCase(setUserInfos.rejected, (_, action) => {
                 console.log("Une erreur est survenue... ", action.payload);
             })
     }
--- src/pages/SignIn/login.tsx
+++ src/pages/SignIn/login.tsx
@@ -1,61 +1,45 @@
-import {FC, FormEvent, useActionState, useEffect, useState} from 'react';
+import {ChangeEvent, FC, useActionState, useEffect, useState} from 'react';
 import './login.scss';
 import {useDispatch, useSelector} from "react-redux";
-import {fetchInfosUser, fetchTokenUser} from "../../core/userSlicer.ts";
-import {useNavigate} from "react-router-dom";
+import {getUserInfos, getUserToken} from "../../core/userSlicer.ts";
+import {NavigateFunction, useNavigate} from "react-router-dom";
 import {AppDispatch} from "../../core/store.ts";
 import {userInfos, userToken} from "../../core/selectors.ts";
 import {UserLogin} from "../../core/interfaces/user-login-interface.ts";
+import {UserInformationInterface} from "../../core/interfaces/user-interface.ts";
+import {SubmitResult} from "../../shared/interfaces/submit-result-interface.ts";
 
 const Login: FC = () => {
-    const [state, formAction, isPending] = useActionState(validForm, {
-        success: false,
-        message: "",
-        error: null,
-    });
+    const [state, submitAction, isPending] = useActionState<SubmitResult | null, FormData>(submitHandler, null);
     const dispatch: AppDispatch = useDispatch();
-    const navigate = useNavigate();
-    const token = useSelector(userToken)
-    const userInfoStored = useSelector(userInfos)
-    const [rememberLogin, setRememberLogin] = useState<string>('');
-    const [rememberCheck, setRememberCheck] = useState<boolean>(false);
+    const navigate: NavigateFunction = useNavigate();
+    const token: string | undefined = useSelector(userToken)
+    const userInfoStored: UserInformationInterface | undefined = useSelector(userInfos);
+    const rememberedEmail: string = localStorage.getItem('rememberUser') ?? '';
+    const [rememberCheck, setRememberCheck] = useState<boolean>(rememberedEmail ? !!rememberedEmail : false);
 
     useEffect(() => {
-
-        if (state.success && token) {
-            dispatch(fetchInfosUser(token));
+        if (token) {
+            dispatch(getUserInfos(token));
         }
 
-        return () => {}
-
-    }, [state.success, token])
+        return () => {};
+    }, [token]);
 
     useEffect(() => {
-        if (userInfoStored.id) {
-            navigate(`/user/${userInfoStored.id}`)
+        if (userInfoStored?.id) {
+            setTimeout(() => {
+                navigate(`/user/${userInfoStored.id}`);
+
+            }, 2000)
         }
 
-        return () => {}
-
+        return () => {};
     }, [userInfoStored]);
 
-    useEffect(() => {
-        const rememberedEmail = localStorage.getItem('rememberUser');
-
-        if (rememberedEmail) {
-            setRememberLogin(rememberedEmail);
-            setRememberCheck(true);
-        }
-
-        return () => {}
-
-    }, [rememberLogin, rememberCheck]);
-
-    function validForm(_: any, data: any) {
-        const email = data.get('email');
-        const isValidEmail = email.includes('@');
-
-        new Promise(success => setTimeout(success, 2000)).then();
+    async function submitHandler(_previousState: SubmitResult | null, formData: FormData): Promise<SubmitResult> {
+        const email: string = formData.get('email') as string;
+        const isValidEmail: boolean = email.includes('@');
 
         if (!isValidEmail) {
             return {
@@ -66,31 +50,33 @@ const Login: FC = () => {
         }
 
         const userForm: UserLogin = {
-            email: data.get('email'),
-            password: data.get('password'),
-            rememberMe: data.get('rememberMe')
+            email: formData.get('email') as string,
+            password: formData.get('password') as string,
+            rememberMe: formData.get('rememberMe') === 'on',
         };
 
-        dispatch(fetchTokenUser(userForm));
+        try {
+            await dispatch(getUserToken(userForm)).unwrap();
 
-        return {
-            success: true,
-            message: 'Succeded authentication, you will be redirected to the homepage...',
-            error: null,
-        };
+            return {
+                success: true,
+                message: "Succeeded authentication, you will be redirected to the homepage...",
+                error: null,
+            };
+        } catch (error) {
+            return {
+                success: false,
+                message: "Can't connect",
+                error: 'Wrong email or password, please retry',
+            };
+        }
     }
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        formAction(formData);
-    }
-
-    const handleRememberChange = (event: any) => {
+    const handleRememberChange = (event: ChangeEvent<HTMLInputElement>) => {
         const isChecked: boolean = event.target.checked;
         setRememberCheck(isChecked);
 
-        if (!isChecked && rememberLogin) {
+        if (!isChecked && rememberedEmail) {
             localStorage.removeItem('rememberUser');
         }
     }
@@ -98,27 +84,34 @@ const Login: FC = () => {
     return (
         <main className="main bg-dark login">
             <section className="sign-in-content">
+                {state && state.success ? (
+                <div className="overlay-login">
+                    <svg width="48" height="48" viewBox="0 0 48 48" className="loader">
+                        <path d="M24 4 A20 20 0 1 1 4 24" stroke="#12002b" strokeWidth="7" fill="none" strokeLinecap="round" />
+                    </svg>
+                </div>
+                ) : null}
                 <i className="fa fa-user-circle sign-in-icon"></i>
                 <h1>Sign-in</h1>
-                <form onSubmit={handleSubmit}>
+                <form action={submitAction}>
                     <div className="input-wrapper">
                         <label htmlFor="email">Email</label>
-                        <input type="text" id="email" name="email" defaultValue={rememberLogin ? rememberLogin : ''} required/>
+                        <input type="text" id="email" name="email" defaultValue={rememberedEmail ? rememberedEmail : ''} disabled={!!userInfoStored?.id} required/>
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" required/>
+                        <input type="password" id="password" name="password" disabled={!!userInfoStored?.id} required/>
                     </div>
                     <div className="input-remember">
-                        <input type="checkbox" id="rememberMe" name="rememberMe" checked={rememberCheck} onChange={handleRememberChange}/>
+                        <input type="checkbox" id="rememberMe" name="rememberMe" checked={rememberCheck} disabled={!!userInfoStored?.id} onChange={handleRememberChange}/>
                         <label htmlFor="rememberMe">Remember me</label>
                     </div>
                     <button className="sign-in-button" type="submit" disabled={isPending}>
                         {isPending ? 'Connexion...' : 'Sign In'}
                     </button>
                 </form>
-                {state.error && <p className="error">{state.error}</p>}
-                {state.success && <p className="success">{state.message}</p>}
+                {state?.message && <p className="success">{state?.message}</p>}
+                {state?.error && <p className="error">{state?.error}</p>}
             </section>
         </main>
     );
